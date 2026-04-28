57d6d328d7c21d5af3cf57b56dcee5bb08d13017
Amadeu Joaquim <amadeuyoung@gmail.com>
2026-04-28 12:04:43 +0200

    feat(webhook): Add webhook endpoint to receive DHIS2 events and trigger automatic audit process

--- core/routes/audit_object_urls.py
+++ core/routes/audit_object_urls.py
@@ -7,6 +7,9 @@ from core.models.models import AuditObject
 from fastapi import Request
 from core.auth.dependencies import get_current_user, require_superuser
 from core.auth.models import User
+from datetime import datetime
+
+
 router = APIRouter()
 
 audit_crud = CRUDBase[AuditObject, AuditObjectCreate, AuditObjectRead](AuditObject)
@@ -35,7 +38,12 @@ def get_audit_objects(request: Request, page: int = 1, pageSize: int = 50, db: S
     excluded = {"page", "pageSize"}
     filters = {k: v for k, v in request.query_params.items() if k not in excluded}
     result = audit_crud.get_all(db, page=page, pageSize=pageSize, filters=filters)
-    data = sorted(result["data"], key=lambda x: x.created_at, reverse=True)
+    # data = sorted(result["data"], key=lambda x: x.created_at, reverse=True)
+    data = sorted(
+        result["data"],
+        key=lambda x: datetime.fromisoformat(x.created_at.replace("Z", "+00:00")),
+        reverse=True
+    )
     return {"pager": result["pager"], "auditObjects": data}
 
 
