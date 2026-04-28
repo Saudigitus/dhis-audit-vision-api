e4d9a44acdea0a97d66b58d31eb9acfdb0251403
Amadeu Joaquim <amadeuyoung@gmail.com>
2026-04-28 11:59:09 +0200

    feat(webhook): Add webhook endpoint to receive DHIS2 events and trigger automatic audit process

--- core/routes/web_hook_urls.py
+++ core/routes/web_hook_urls.py
@@ -29,7 +29,7 @@ async def receive_dhis2_event_webhook(request: Request):
         print(f"Received DHIS2 webhook: {payload}")
 
         resource_uid = payload.get("path", "").split(".")[2]
-        created_at = payload.get("createdAt")
+        created_at = payload.get("createdAt").replace("T", " ")
 
         audit_process.automatic_run(RETRIEVE_SQL_VIEW_ID, resource_uid, created_at)
 
