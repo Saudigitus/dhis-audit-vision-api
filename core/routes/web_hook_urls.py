3ec100311b5e6faa0c4107c393ee0ca7711d919a
Amadeu Joaquim <amadeuyoung@gmail.com>
2026-04-28 11:54:08 +0200

    feat(webhook): Add webhook endpoint to receive DHIS2 events and trigger automatic audit process

--- core/routes/web_hook_urls.py
+++ core/routes/web_hook_urls.py
@@ -17,7 +17,7 @@ router = APIRouter()
 
 
 @router.post("/dhis2/event")
-async def receive_dhis2_event_webhook(request: Request, _: User = Depends(get_current_user)):
+async def receive_dhis2_event_webhook(request: Request, _: User = Depends(require_superuser)):
     """
     Webhook endpoint to receive events from DHIS2.
     """
